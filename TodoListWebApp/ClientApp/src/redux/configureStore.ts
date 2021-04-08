60bf7328e364b2cdcf0ca2c88ed6bcd909e2069f
Vladislav Avanesov <avaneszdes@gmail.com>
2021-04-08 17:42:52 +0300

    add: error reducer

--- TodoListWebApp/ClientApp/src/redux/configureStore.ts
+++ TodoListWebApp/ClientApp/src/redux/configureStore.ts
@@ -6,16 +6,18 @@ import {composeWithDevTools } from 'redux-devtools-extension';
 import {rootSaga} from "../sagas/root";
 import auth from "./auth-reducer";
 import admin from "./admin-reducer";
+import error from "./error-reducer";
 
 export interface IRootState {
     auth: Auth,
     todos: ITodosState ,
     admin: User[],
+    error: string,
 }
 
 export default function configureStore() {
     const sagaMiddleware = createSagaMiddleware()
-    const todoApp = combineReducers({todos, auth, admin})
+    const todoApp = combineReducers({todos, auth, admin,error})
     const store = createStore(todoApp,  composeWithDevTools(applyMiddleware(sagaMiddleware)))
     sagaMiddleware.run(rootSaga)
     
