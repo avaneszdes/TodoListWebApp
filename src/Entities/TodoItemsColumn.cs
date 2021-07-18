1fa57a0091791042691a50d8cf48d0019f317c5f
Avanesov Vladislav <avaneszdes@gmail.com.com>
2021-07-18 22:52:23 +0300

    feat: add class TodoItemsColumn.cs and fix some files

---
+++ src/Entities/TodoItemsColumn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Entities
+{
+    public class TodoItemConcrete : BaseEntity
+    {
+        public string TodoItemListName { get; set; }
+
+        public List<TodoItem> TodoItems = new ();
+        
+        public long UserId { get; set; }
+        public User User { get; set; }
+        
+    }
+}
